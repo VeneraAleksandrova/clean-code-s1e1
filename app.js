3b2476e6fb30a9e2c868397811a00280921942b2
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2025-10-20 23:06:35 +0300

    refactor: add alt in img tags according to rule 1.2 in html-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.classList.add("button","delete")
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className="delete-img"
+  deleteButtonImg.alt="basket";
   deleteButton.appendChild(deleteButtonImg);
 
 
